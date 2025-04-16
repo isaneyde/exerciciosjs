4494107777777b29523d97db262cd9b280351faa
isaneyde <issitoe941@gmail.com>
2025-04-16 16:10:43 +0200

    update the list funtions

---
+++ a7.js
@@ -0,0 +1,10 @@
+let nome = prompt("Insira o seu nome");
+let apelido = prompt("Insira o seu apelido");
+function nomes(nome, apelido) {
+    return pessoa = {
+        nome: `O nome da pessoa é ${nome}`,
+        apelido: `O apelido da pessoa é ${apelido}`,
+        nomeCompleto: nome + " " + apelido,
+    }
+}
+alert(nomes(nome, apelido));
